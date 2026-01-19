7332a28e7870a51ef66541a6b4a62177f0820f45
Bruno Paes e lima <brunopaeselima@Brunos-MacBook-Pro.local>
2026-01-19 01:03:37 -0500

    fix: Simplify vite.config.ts to use relative paths for HTML entry points

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,9 +12,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(fileURLToPath(new URL('.', import.meta.url)), 'index.html'),
-        login: resolve(fileURLToPath(new URL('.', import.meta.url)), 'login.html'),
-        agency: resolve(fileURLToPath(new URL('.', import.meta.url)), 'agency.html')
+        main: './index.html',
+        login: './login.html',
+        agency: './agency.html'
       }
     }
   }
