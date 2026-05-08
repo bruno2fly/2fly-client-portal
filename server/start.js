1dfd853c285a7848f7140bc2370289a4a4095d37
Bruno <2flydigitalmarketing@gmail.com>
2026-05-08 08:39:18 -0400

    fix: restore real server start command, remove diagnostic start.js

--- server/start.js
+++
@@ -1,6 +0,0 @@
-import http from 'http';
-const PORT = process.env.PORT || 3001;
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from start.js');
-}).listen(PORT, () => console.log('Listening on ' + PORT));
